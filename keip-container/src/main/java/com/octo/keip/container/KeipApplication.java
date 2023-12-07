6a141f0ee55b1c723770c35ea8196844470ebdae
Matthew Calcote <11355332+mcalcote@users.noreply.github.com>
2023-12-07 10:10:35 -0700

    Re-packaged keip (#3)
    * Re-packaged keip
    
    * Shortened package name
    
    * fixed broken urls
    
    * updated directory
    
    ---------
    
    Co-authored-by: Matthew Calcote <matthew.calcote@octo.us>

--- keip-container/src/main/java/com/octo/keip/container/KeipApplication.java
+++ keip-container/src/main/java/com/octo/keip/container/KeipApplication.java
@@ -1,4 +1,4 @@
-package com.connexta.keip.container;
+package com.octo.keip.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
