7a7e285a13e936dbc7b00c9d0c5bb0606376d8a6
Andrew <andrew.pelletier@octo.us>
2024-08-01 16:38:46 -0400

    comment out other jobs and print target branch

--- .github/workflows/scripts/verify_operator_version.sh
+++ .github/workflows/scripts/verify_operator_version.sh
@@ -1,18 +1,21 @@
 set -eux
 
-OPERATOR_CONTROLLER_YAML=operator/controller/integrationroute-controller.yaml
+OPERATOR_DIR=operator
+OPERATOR_CONTROLLER_YAML=$OPERATOR_DIR/controller/integrationroute-controller.yaml
 
-install_dependencies() {
-  # install yq
-  echo installing
-}
 
 verify_current_webhook_img() {
   current_webhook_img=$(make --no-print-directory -C operator/webhook get-image-name)
   webhook_image_used=$(yq eval '.spec.template.spec.containers[].image' $OPERATOR_CONTROLLER_YAML)
 
+  test -n "$current_webhook_img"
+  test -n "$webhook_image_used"
+
   test "$webhook_image_used" = "$current_webhook_img"
 }
 
-install_dependencies
+verify_operator_version_bump() {
+  git diff main $OPERATOR_DIR
+}
+
 verify_current_webhook_img
