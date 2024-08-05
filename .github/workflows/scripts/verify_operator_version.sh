0d3cbeda67e32f1e42994e5d4014d66a08ae8a2c
Andrew <andrew.pelletier@octo.us>
2024-08-05 11:34:15 -0400

    split version check into separate script

--- .github/workflows/scripts/verify_operator_version.sh
+++ .github/workflows/scripts/verify_operator_version.sh
@@ -14,19 +14,8 @@ verify_current_webhook_img() {
 }
 
 verify_operator_version_bump() {
-  # github actions job does not fetch other git objects by default
-  git fetch origin $GITHUB_BASE_REF
-  git fetch --tags
-
-  changes_in_pr=$(git diff origin/$GITHUB_BASE_REF -- $OPERATOR_DIR)
-  if [ -n "$changes_in_pr" ]; then
-    echo "Found changes between current branch and $GITHUB_BASE_REF"
-    potential_tag=$(make --no-print-directory -C $OPERATOR_DIR get-tag)
-    if git tag | grep -x "$potential_tag"; then
-      echo "$potential_tag was already released. Please increment the version if changes were made to the operator."
-      exit 1
-    fi
-  fi
+  potential_tag=$(make --no-print-directory -C $OPERATOR_DIR get-tag)
+  sh .github/workflows/scripts/verify_changes_update_version.sh $potential_tag $OPERATOR_DIR
 }
 
 verify_current_webhook_img
